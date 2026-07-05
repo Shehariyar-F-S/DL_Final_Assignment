24f2240b9cfc58aa6db4356a972c83acaee10b94
Shehariyar-F-S <sher29.dev@gmail.com>
2026-07-05 23:23:38 +0200

    fix : data.py fixed the data leak issue for validation data and labels

--- Code/data.py
+++ Code/data.py
@@ -24,8 +24,8 @@ def get_loaders(data, data_path, batch_size, val_split=0.1):
 
     train_data = shuffled_images[:val_start]   # fix: data leak issue fixed for train data and labels
     train_labels = shuffled_labels[:val_start]
-    val_data = data_dict['train_images'][val_start:]
-    val_labels = data_dict['train_labels'][val_start:]
+    val_data = shuffled_images[val_start:]   # fix: data leak issue fixed for validation data and labels
+    val_labels = shuffled_labels[val_start:]
     
     train_dataset = TensorDataset(train_data, train_labels)
     val_dataset = TensorDataset(val_data, val_labels)
