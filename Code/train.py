f657cf63563d3400a60cacd011efd28809f19a71
Shehariyar-F-S <sher29.dev@gmail.com>
2026-07-06 00:59:32 +0200

    fix:train.py fixed the indentation error inside the 2nd for loop

--- Code/train.py
+++ Code/train.py
@@ -30,18 +30,21 @@ def main():
             channels = dataset_config["CHANNELS"]
             num_classes = dataset_config["NUM_CLASSES"] # enhancement:added the configuration for the model to use.
 
-    train_loader, val_loader, test_loader = get_loaders(data=config["DATA"],  # fix: fixed syntax error for var test_loader
+            train_loader, val_loader, test_loader = get_loaders(data=config["DATA"],  # fix: fixed syntax error for var test_loader
                                                         data_path=config["DATA_PATH"], 
                                                         batch_size=config["BATCH_SIZE"],
                                                         val_split=config.get("VAL_SPLIT", 0.1)) #added val_split
 
-    model_class = getattr(models, config["MODEL"])
-    model = model_class(in_channels=channels, num_classes=num_classes).to(device) #fix: removed the 0.99 dropout and intialized the model with the correct number of input channels and classes
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=config["LEARNING_RATE"])
+            model_class = getattr(models, config["MODEL"])
+            model = model_class(in_channels=channels, num_classes=num_classes).to(device) #fix: removed the 0.99 dropout and intialized the model with the correct number of input channels and classes
+            criterion = nn.CrossEntropyLoss()
+            optimizer = optim.Adam(model.parameters(), lr=config["LEARNING_RATE"])
 
-    trainer = Trainer(model, criterion, optimizer, device)
-    trainer.fit(train_loader, val_loader, epochs=config["EPOCHS"], dataset_name=data_name)  # fix: added dataset_name parameter to the fit method for better logging
+            trainer = Trainer(model, criterion, optimizer, device)
+            trainer.fit(train_loader, val_loader, epochs=config["EPOCHS"], dataset_name=data_name)  # fix: added dataset_name parameter to the fit method for better logging
+            
+
+    
 
 if __name__ == "__main__":
     main()
